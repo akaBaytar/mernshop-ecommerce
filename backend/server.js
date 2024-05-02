97c28d58e6fa36a998596d4e49a4b8f8dbe636ba
Burak Bilgili <burak.bilgili@icloud.com>
2024-05-02 14:03:27 +0300

    setup mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import connectDatabase from './config/database.js';
+
 dotenv.config();
 
 import PRODUCTS from './mock/products.js';
 
 const port = process.env.PORT || 5000;
 
+connectDatabase(); // mongoDB
+
 const app = express();
 
+// routes
 app.get('/', (_, res) => res.send('API is running...'));
 
 app.get('/api/v1/products', (_, res) => {
