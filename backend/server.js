3badb24fae2cae85a09e06f6c04ea7d1caa125ea
Burak Bilgili <burak.bilgili@icloud.com>
2024-05-02 17:35:07 +0300

    create error handler function and setup server with that

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import connectDatabase from './config/database.js';
 
 import product from './routes/product.js';
 
+import { notFound, errorHandler } from './error/errorHandler.js';
+
 dotenv.config();
 
 const port = process.env.PORT || 5000;
@@ -17,6 +19,10 @@ app.get('/', (_, res) => res.send('API is running...'));
 
 // routes
 app.use('/api/v1/products', product);
+
+// errors
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
