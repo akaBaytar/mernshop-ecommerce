61b7832ad7bd7c9f3c42debff0738f22aeb34a07
Burak Bilgili <burak.bilgili@icloud.com>
2024-05-05 15:55:36 +0300

    create cart screen and helper functions

--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -8,12 +8,14 @@ import App from '../App.jsx';
 
 import Home from '../screens/Home.jsx';
 import Product from '../screens/Product.jsx';
+import Cart from '../screens/Cart.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route index={true} path='/' element={<Home />} />
       <Route path='/products/:id' element={<Product />} />
+      <Route path='/cart' element={<Cart />} />
     </Route>
   )
 );
