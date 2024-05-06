59e792cf54648061de2445a445959d90a596b02d
Burak Bilgili <burak.bilgili@icloud.com>
2024-05-06 20:11:36 +0300

    register screen and functionality

--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -10,6 +10,7 @@ import Home from '../screens/Home.jsx';
 import Product from '../screens/Product.jsx';
 import Cart from '../screens/Cart.jsx';
 import Login from '../screens/Login.jsx';
+import Register from '../screens/Register.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -18,6 +19,7 @@ const router = createBrowserRouter(
       <Route path='/products/:id' element={<Product />} />
       <Route path='/cart' element={<Cart />} />
       <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
     </Route>
   )
 );
---
+++ frontend/src/screens/Register.jsx
@@ -0,0 +1,120 @@
+import { useState, useEffect } from 'react';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { Form, Button, Row, Col, Spinner } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
+
+import FormContainer from '../components/FormContainer';
+
+import { useRegisterMutation } from '../slices/usersApiSlice';
+import { setCredentials } from '../slices/authSlice';
+
+const Register = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [register, { isLoading }] = useRegisterMutation();
+
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const { search } = useLocation();
+  const params = new URLSearchParams(search);
+  const redirect = params.get('redirect') || '/';
+
+  useEffect(() => {
+    if (userInfo) navigate(redirect);
+  }, [userInfo, redirect, navigate]);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      toast.error('Passwords do not match.');
+    } else {
+      try {
+        const res = await register({ name, email, password }).unwrap();
+        dispatch(setCredentials({ ...res }));
+        navigate(redirect);
+      } catch (error) {
+        toast.error(error?.data?.message || error?.error);
+      }
+    }
+  };
+
+  return (
+    <FormContainer>
+      <h1>Register</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId='name' className='my-3'>
+          <Form.Label>Name:</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Name'
+            autoComplete='name'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group controlId='email' className='my-3'>
+          <Form.Label>Email:</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Email address'
+            autoComplete='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group controlId='password' className='my-3'>
+          <Form.Label>Password:</Form.Label>
+          <Form.Control
+            type='password'
+            value={password}
+            autoComplete='none'
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group controlId='confirmPassword' className='my-3'>
+          <Form.Label>Confirm Password:</Form.Label>
+          <Form.Control
+            type='password'
+            value={confirmPassword}
+            autoComplete='none'
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </Form.Group>
+
+        <div className='d-grid'>
+          <Button
+            type='submit'
+            variant='dark'
+            className='mt-3 py-2'
+            disabled={isLoading}>
+            {isLoading ? (
+              <Spinner style={{ height: '1rem', width: '1rem' }} />
+            ) : (
+              'Sign Up'
+            )}
+          </Button>
+        </div>
+      </Form>
+      <Row className='py-3'>
+        <Col>
+          <small>
+            Already have an account?{' '}
+            <Link to={redirect ? `/login?redirect=${redirect}` : '/login'}>
+              Login here.
+            </Link>
+          </small>
+        </Col>
+      </Row>
+    </FormContainer>
+  );
+};
+
+export default Register;
