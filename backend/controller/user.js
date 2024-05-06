298c204051d7692c8162bbf44eaf17e8f61a97fa
Burak Bilgili <burak.bilgili@icloud.com>
2024-05-06 16:58:45 +0300

    get user profile and update user profile functionality

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -13,7 +13,7 @@ const authUser = asyncHandler(async (req, res) => {
   if (user && (await user.matchPassword(password))) {
     generateToken(res, user._id);
 
-    res.json({
+    res.status(200).json({
       _id: user._id,
       name: user.name,
       email: user.email,
@@ -75,14 +75,49 @@ const logoutUser = asyncHandler(async (req, res) => {
 // @route     GET /api/v1/users/profile
 // @access    private
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send('get user profile');
+  const id = req.user._id;
+  const user = await User.findById(id);
+
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found.');
+  }
 });
 
 // @desc      update user profile
 // @route     PUT /api/v1/users/profile
 // @access    private
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.send('update user profile');
+  const id = req.user._id;
+  const user = await User.findById(id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.user.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found.');
+  }
 });
 
 // @desc      get all users
